bc8a9178565b1d87501331d277a1df8f64dcd2d0
Akash7053 <Aakashssingh9988@gmail.com>
2025-12-27 12:04:45 -0500

    "New java docs added"

--- src/String_programs/Reverse_String.java
+++ src/String_programs/Reverse_String.java
@@ -5,10 +5,12 @@ import java.util.stream.IntStream;
 
 public class Reverse_String {
     public static void main(String[] args) {
+        // Using reduce method
         String string = "Akash";
         String revString = string.chars().mapToObj(c -> (char) c).map(String::valueOf).reduce("", (a, b) -> b + a);
         System.out.println(revString);
 
+        // Using IntStream and collect
         String string1 = "Aaksh";
         String rev = IntStream.rangeClosed(1, string1.length())
                 .mapToObj(i -> string1.charAt(string1.length() - i))
@@ -16,12 +18,13 @@ public class Reverse_String {
                 .collect(Collectors.joining());
         System.out.println(rev);
 
-
+      // Using character array
         String string2 = "Akash";
         char[] chr = string2.toCharArray();
         for (int i = chr.length - 1; i >= 0; i--) {
             System.out.println(chr[i]);
         }
+       // Directly looping through string lentgh
         String string3 = "Akash";
         for (int i = string3.length() - 1; i >= 0; i--) {
             System.out.println(string2.charAt(i));
